f5e49a2f752f3399166a9f5cf2bb80da722759c1
yedoka <akedilbekbolatov@gmail.com>
2024-10-19 19:53:18 +0200

    style: margin from top and left

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -4,37 +4,9 @@ import './Widget.css';
 
 const Widget = () => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const [hasInput, setHasInput] = useState(false);
-  const [inputValue, setInputValue] = useState(''); 
-  const [isEditing, setIsEditing] = useState(false); 
-  const [errorMessage, setErrorMessage] = useState('');
 
   const handleClick = () => {
     setIsExpanded(true);
-    if (!hasInput || isEditing) {
-      console.log('Input required or editing input');
-    } else {
-      console.log('Skip input, go directly to menu');
-    }
-  };
-
-  const handleInputSubmit = (e) => {
-    e.preventDefault();
-    const parsedValue = parseInt(inputValue, 10);
-
-    if (isNaN(parsedValue) || parsedValue < 0) {
-      setErrorMessage('Please enter a valid positive integer.');
-    } else {
-      setHasInput(true); 
-      setIsEditing(false);  
-      setIsExpanded(true);  
-      setErrorMessage('');  
-    }
-  };
-
-  const handleEditInput = () => {
-    setIsEditing(true);  
-    setIsExpanded(true); 
   };
 
   return (
@@ -43,35 +15,16 @@ const Widget = () => {
         <img src={commerzduck} alt="commerzduck" />
         <button className={`widget__button ${isExpanded ? 'hidden' : ''}`} onClick={handleClick}>Accountant</button>
 
-        {(!hasInput || isEditing) && isExpanded && (
-        <form onSubmit={handleInputSubmit} className="input__form">
-          <label htmlFor="inputField" className='input__form__label'>Please input the companies salary:</label>
-          <input 
-            id="inputField" 
-            type="number" 
-            value={inputValue} 
-            onChange={(e) => setInputValue(e.target.value)} 
-            min="0"
-            className='input__form__input'
-          />
-          {errorMessage && <p className="error-message">{errorMessage}</p>}
-          <button type='submit' className='widget__button'>Submit</button>
-        </form>
-      )}
-
-      {hasInput && !isEditing && isExpanded && (
-        <div>
-          <ul className="widget__options">
-            <li><a href="#" className='widget__option'>Overview</a></li>
-            <li><a href="#" className='widget__option'>Forms and Fill</a></li>
-            <li><a href="#" className='widget__option'>Debt Check</a></li>
-            <li><a href="#" className='widget__option'>History</a></li>
-          </ul>
-          <div className='widget__salary'>
-          <p>On a salary of <span onClick={handleEditInput}> {inputValue}€ Back</span></p>
+        {isExpanded && (
+          <div>
+            <ul className="widget__options">
+              <li><a href="#" className='widget__option'>Overview</a></li>
+              <li><a href="#" className='widget__option'>Forms and Fill</a></li>
+              <li><a href="#" className='widget__option'>Debt Check</a></li>
+              <li><a href="#" className='widget__option'>History</a></li>
+            </ul>
           </div>
-        </div>
-      )}
+        )}
       </div>
     </div>
   );
